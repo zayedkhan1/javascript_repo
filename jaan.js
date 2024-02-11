6bc85322253e1e0d574a9543d4f92a81de5ec149
zayed khan <zayedk458@gmail.com>
2024-02-11 16:40:37 +0600

    operations

--- jaan.js
+++ jaan.js
@@ -1,1 +1,27 @@
-console.log(555);
+//variable--> var
+//numeric type
+var price =65;
+var age=45;
+//string type
+var name ="sodor uddin";
+ var location ='Bandarban achi vai';
+ //boolean
+  var serious=true;
+  var isFail=false;
+  //
+  var prices=23;
+var person="tony stark";
+// var my_home_town_is_dhaka ="dhaka";//snckecase
+var myHomeTownIs="Dhaka";//camelcase
+
+//operation (addintion,substraction)
+var oninoPrice=5;
+var eggprice=15;
+// var totalPrice=oninoPrice+eggprice;
+// var totalPrice=eggprice-oninoPrice;
+
+// multiplicatiion,division
+var orangePrice=34;
+var quantity= 5;
+var totar_price= orangePrice/quantity;
+console.log(totar_price)
---
+++ math.js
@@ -0,0 +1,19 @@
+/*
+// integer,float,
+// operation:addition,multiplication,divide,modlue,substraction.
+
+1.avarage
+2.percentage
+3.comparison: >, <, ==, !=, >=, =<
+4.oikin niyom: 2kg apple and 5 kg angur 
+6. 45,67,65,3,65 largest,maximum,minimum,avg,sum,sub,smallest
+7. series: odd,even
+8. prime number
+9.permutation
+10.numbers
+11.Area
+
+:::Always,jokhoni lage tokhoni ami sikhe nibo:::
+
+
+*/
