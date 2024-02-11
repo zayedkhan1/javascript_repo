1eb18335d97e1699ce0a872895768bf3596a529e
zayed khan <zayedk458@gmail.com>
2024-02-11 20:26:50 +0600

    timato

--- math.js
+++ math.js
@@ -13,7 +13,17 @@
 10.numbers
 11.Area
 
-:::Always,jokhoni lage tokhoni ami sikhe nibo:::
+:::Always,jokhoni lage tokhoni amake sikhe nite hobe:::
 
 
+*/
+/*
+var tomatoPrice=45;
+tomatoPrice=55;
+console.log(tomatoPrice)
+
+var price1=31;
+var price2=64;
+var totalPrice=price1+price2;
+console.log("Yoru total price: " + totalPrice)
 */
