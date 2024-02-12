551d8952b647c4569d6e135c870af585edd6cba9
zayed khan <zayedk458@gmail.com>
2024-02-12 18:55:06 +0600

    comment

--- math.js
+++ math.js
@@ -27,3 +27,48 @@ var price2=64;
 var totalPrice=price1+price2;
 console.log("Yoru total price: " + totalPrice)
 */
+
+/*
+//print fullname
+var firstName="tony";
+var lastName=" stark";
+var fullName=firstName+lastName;
+console.log(fullName)
+/*
+/*
+//convert strign to number
+var price1='12';
+var price2=' 34';
+var totalPrice=price1+price2;
+console.log(totalPrice)
+console.log(price1)
+var price1Numbe=parseInt(price1)
+console.log(price1)
+*/
+/*
+var price=299;
+// console.log(typeof price)
+var isHappy=true;
+// console.log(typeof isHappy)
+var romantic;
+// console.log(typeof romantic)
+/*
+//using toFixed function
+var num1=0.1;
+var num2=0.2;
+var sum= num1+num2;
+ sum=sum.toFixed(3);
+ sum=parseFloat(sum);
+console.log(sum)
+*/
+
+
+/*//calculating reminder (%)
+
+var mangoes= 19;
+var person =7;
+var reminder=mangoes%person;
+console.log(reminder)
+*/
+
+//problem solving
