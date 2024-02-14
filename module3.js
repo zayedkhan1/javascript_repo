ef62642500f8b731de89ada5f8196176bcde6990
zayed khan <zayedk458@gmail.com>
2024-02-14 21:23:10 +0600

    second commit

---
+++ module3.js
@@ -0,0 +1,41 @@
+//function declaration1
+/* 
+function startType(){
+    console.log('stand up');
+    console.log('walk to the swithc');
+    console.log('press the switch');
+    console.log('akhon aisha ghuma');
+}
+//calling function
+startType();
+startType();
+startType();
+ */
+//fucntion declaration 2
+/* 
+function startSingara(mama){
+    console.log("mama singara den");
+    console.log(mama);
+}
+startSingara(1); */
+//Function 3 using parameter
+/* 
+function bringWater(mama){
+    console.log("vai ai ne apnr ",mama);
+    console.log("ai nen pani");
+}
+var taka=200;
+bringWater(taka); */
+// use akhadik perameter
+/* 
+function  add(number1,number2){
+    console.log("going to add ",number1,number2);
+}
+add(34,56); */
+/* 
+function sum(a,b,c,d,e){
+    console.log(a,b,c,d,e);
+    var sum=a+b+c+d+e;
+    console.log(sum);
+}
+sum(4, 6, 8, 4, 3, 2); */
