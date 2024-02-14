fc5ef6a7dfb9e8c7c32ecdd122a3e6016f294937
zayed khan <zayedk458@gmail.com>
2024-02-14 23:03:28 +0600

    third repo

--- module3.js
+++ module3.js
@@ -39,3 +39,58 @@ function sum(a,b,c,d,e){
     console.log(sum);
 }
 sum(4, 6, 8, 4, 3, 2); */
+//using return
+/* 
+function myNumber(num1,num2){
+    console.log(num1,num2);
+    var sum=num1+num2;
+    return sum;
+    return 12;
+}
+var total = myNumber(30,40);
+console.log('total',total) */
+// problem sollution by function
+/* 
+function bringSingara(money){
+    var singaraPrice=10;
+    var sigaraQuantity=money/singaraPrice;
+    return sigaraQuantity;
+}
+var myTaka=500;
+var totalSingara=bringSingara(myTaka);
+console.log(totalSingara); */
+//
+/* 
+function pitha(money){
+    var pithaQuantity=10;
+    var totalPithaPrice=pithaQuantity*money;
+    return totalPithaPrice;
+}
+var taka=244;
+var price=pitha(taka);
+console.log(price);
+ */
+//
+/* 
+function getTotal(assaignment1,assaignment2,assaignment3){
+    var total=assaignment1+assaignment2+assaignment3;
+    var avg=total/3;
+    return avg;
+}
+var assaignment1=34;
+var assaignment2=54;
+var assaignment3=78;
+
+var myAvg=getTotal(assaignment1,assaignment2,assaignment3);
+console.log(myAvg); */
+//
+/* 
+function add(num1,num2){
+    var sum=num1+num2;
+    return sum;
+
+}
+var reasult1=add(5,6);
+var reasult2=add(7,8);
+var totalReasult=add(reasult1,reasult2);
+console.log(totalReasult); */
